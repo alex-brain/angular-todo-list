10def75b5c4ef6cce0fc8b4ff9812d01f83d3dd0
Alex <cm-brain@yandex.ru>
2017-06-26 15:23:06 +0300

    Added task editor component

---
+++ src/pages/task-editor/task-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'task-editor',
+  templateUrl: './task-editor.component.html',
+  styleUrls: ['./task-editor.component.css']
+})
+export class TaskEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
